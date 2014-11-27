de988e8aa98328657f195d44c0559a914e167c2e
Dave Ferrante <wavy2112@user.noreply.github.com>
2014-11-27 15:45:30 -0800

    updates to biz and EF projects

---
+++ proDev.Biz/CoreEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Spatial;
+
+namespace proDev.Biz
+{
+    public class CoreEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DbGeography GeoPoly { get; set; }
+
+        public static List<CoreEntity> GetCoreEntities() {
+
+            List<CoreEntity> coreEntities = new List<CoreEntity>();
+            /*
+            var x = new proDev.EF.PRODEVEntities();
+            var y = x.COREENTITies;
+            */
+
+            
+            
+            return coreEntities;
+
+        }
+    }
+}
