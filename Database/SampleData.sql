ee9644f9f71fdd25322b5ce5b5d5b6d7d39e131f
Dave Ferrante <wavy2112@user.noreply.github.com>
2014-12-04 10:48:57 -0800

    renamed TestData.sql

---
+++ Database/SampleData.sql
@@ -0,0 +1,19 @@
+﻿begin
+DECLARE @g geography;
+
+/* Insert a core entity with multiple polygons */
+SET @g = geography::STMPolyFromText('MULTIPOLYGON(((-122.358 47.653, -122.348 47.649, -122.358 47.658, -122.358 47.653)), ((-122.341 47.656, -122.341 47.661, -122.351 47.661, -122.341 47.656)))', 4326);
+SELECT @g.ToString();
+
+INSERT INTO [dbo].[COREENTITY]
+           ([NAME]
+           ,[GEOPOLY]
+           ,[DATEBEGIN]
+           ,[DATEEND])
+     VALUES
+           ('Multiple Polys TestEntity'
+           ,geography::STMPolyFromText('MULTIPOLYGON(((-122.358 47.653, -122.348 47.649, -122.358 47.658, -122.358 47.653)), ((-122.341 47.656, -122.341 47.661, -122.351 47.661, -122.341 47.656)))', 4326)
+           ,GETUTCDATE()
+           ,DATEADD(YEAR, 3, GETUTCDATE()))
+
+end
