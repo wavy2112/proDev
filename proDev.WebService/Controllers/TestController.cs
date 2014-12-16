f45402b4e6c3ab0c573c4d159074da92152119ec
Dave Ferrante <wavy2112@users.noreply.github.com>
2014-12-15 16:46:56 -0800

    Web service gets EF data and returns on end point

--- proDev.WebService/Controllers/TestController.cs
+++ proDev.WebService/Controllers/TestController.cs
@@ -5,14 +5,29 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using proDev.Biz;
+
 namespace proDev.WebService.Controllers
 {
     public class TestController : ApiController
     {
         // GET <controller>
-        public IEnumerable<string> Get()
+        public List<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            double areaSum = 0;
+            List<string> entities = new List<string>();
+
+            //            List<CoreEntity> coreEntities = CoreEntity.GetCoreEntities();
+
+            List<CoreEntity> coreEntities = CoreEntity.GetAdjacentEntities(2362, 10000);
+
+            foreach (var entity in coreEntities)
+            {
+                entities.Add("Entity: " + entity.Name + " | Area: " + entity.GeoPoly.Area.Value + " | " + entity.BeginDate.ToString() + " to " + entity.EndDate.ToString());
+                areaSum += entity.GeoPoly.Area.Value;
+            }
+            
+            return entities;
         }
 
         // GET <controller>/5
