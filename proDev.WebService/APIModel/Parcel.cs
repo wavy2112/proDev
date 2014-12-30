96ae1eefd5b92591229f1351dc0974a66e9f7080
Dave Ferrante <wavy2112@users.noreply.github.com>
2014-12-30 11:59:44 -0800

    Generate GeoJSON and display on Google Maps

--- proDev.WebService/APIModel/Parcel.cs
+++ proDev.WebService/APIModel/Parcel.cs
@@ -14,5 +14,33 @@ namespace proDev.WebService.APIModel
         public double Area { get; set; }
         public string GML { get; set; }
         public string WKT { get; set; }
+        public string GeoJSON { 
+
+            get{
+                List<string> coordList;
+                string coordString = string.Empty;
+                string[] coordPair = new string[2];
+                string geoJSONCoords;
+
+                coordString = WKT.Substring(WKT.IndexOf("(((") + 3);
+                coordString = coordString.Substring(0, coordString.IndexOf(")))"));
+                coordList = coordString.Split(',').ToList();
+
+                //Create a string of the form
+                //"[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]"
+                geoJSONCoords = string.Empty;
+                foreach (var coord in coordList)
+                {
+                    coordPair = coord.Trim().Split(' ');
+                    if (geoJSONCoords.Length > 0) geoJSONCoords = geoJSONCoords + ",";
+                    geoJSONCoords = geoJSONCoords + "[" + coordPair[0] + "," + coordPair[1] + "]";
+                }
+
+                return "{ \"type\": \"MultiPolygon\"," +
+                            "\"coordinates\":[[[" +
+                            geoJSONCoords +
+                            "]]]}";
+            }
+        }
     }
 }
